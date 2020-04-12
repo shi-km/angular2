67a1aff93be500873bee2ddad7ff05599f05d043
george kamau <flexkamau646@gmail.com>
2020-04-12 21:54:48 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Github Search
+# Description
+A web application for performing github searches using usernames and repositories.
+#### By *John Gichuhi
+
+## Setup/Installation Requirements
+* Clone this repo to your machine
+* `git clone https://github.com/njoroge33/github-search`
+* `cd into github-search`
+* Navigate to src  and open it on your favorite browser
+Your browser must be Angular and node.js enabled for this to work
+
+## Known Bugs
+No known bugs at the moment
+
+## Technologies Used
+* Angular
+* JavaScript
+* HTML
+* CSS
+* Bootstrap
+
+
+### License
+* MIT
+`Copyright (c) 2019 John Gichuhi github-search`
---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  user: any;
+  userRepos:any;
+  username: string = 'shi-km'
+  imageWidth: number = 140;
+  imageHeight: number = 190;
+
+  constructor(private profileService: ProfileService) { 
+    
+  }
+
+  findUser () {
+    this.profileService.UpdateUser(this.username);
+
+    this.profileService.getUser().subscribe(user => {
+      console.log(user);
+      this.user = user;
+    });
+
+    this.profileService.getUserRepos().subscribe(repos => {
+      console.log(repos);
+      this.userRepos = repos;
+    })
+  }
+
+  ngOnInit() {
+    this.findUser()
+  }
+
+}
